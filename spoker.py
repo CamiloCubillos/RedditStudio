9fb63098de58df35b212ab3afb0f7f57de389e29
CamiloCubillos <ccubillosreyes1@gmail.com>
2022-09-16 15:25:08 -0500

    Added option for generating a .txt file with all the stories and autors selected for the session.

--- spoker.py
+++ spoker.py
@@ -1,19 +1,19 @@
 from deep_translator import GoogleTranslator
 from gtts import gTTS
 
+
 class Spoker:
-    def __init__(self,src_text:str):
+    def __init__(self, src_text: str):
         self.src_text = src_text
         self.trans_text = None
         self.audio = None
         self.filename = "gtts.mp3"
 
     def speak(self):
-        translator = GoogleTranslator(source="en",target="es")
+        translator = GoogleTranslator(source="en", target="es")
         self.trans_text = translator.translate(self.src_text)
-        self.audio = gTTS(self.trans_text,lang="es-ES")
+        self.audio = gTTS(self.trans_text, lang="es-ES")
 
     def save_mp3(self):
         self.speak()
         self.audio.save(f"{self.filename}.mp3")
-
